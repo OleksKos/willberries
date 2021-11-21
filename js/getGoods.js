e4eede71479cb07d0f02a32ea328f5bd71f4eb7f
gidroadmin <maindmn@gmail.com>
2021-11-21 20:44:56 +0200

    Added Filter of goods

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,23 +1,61 @@
 const getGoods = () => {
   //First - discribe the varibles
   const links = document.querySelectorAll('.navigation-link');
+
+  const renderGoods = (goods) => {
+    console.log(goods);
+    const goodsContainer = document.querySelector('.long-goods-list');
+    goodsContainer.innerHTML = '';
+    goods.forEach(good => {
+      const goodBlock = document.createElement('div');
+      goodBlock.classList.add('col-lg-3');
+      goodBlock.classList.add('col-sm-6');
+      goodBlock.innerHTML = `
+        <div class="goods-card">
+          <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+          <img src="db/${good.img}" alt="i${good.name}" class="goods-image">
+          <h3 class="goods-title">${good.name}</h3>
+          <p class="goods-description">${good.description}</p>
+          <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+            <span class="button-price">$${good.price}</span>
+          </button>
+        </div>
+      `
+      goodsContainer.append(goodBlock);
+    })
+  }
+
   //The second - discribe functions
-  const getData = () => {
+  const getData = (value, category) => {
     fetch('https://willberries-3d6c2-default-rtdb.firebaseio.com/db.json')
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        const array = category ? data.filter((item) => item[category] === value) : data;
+
+        localStorage.setItem('goods', JSON.stringify(array));
+
+        if (window.location.pathname !== '/goods.html') {
+          window.location.href = '/goods.html';
+        } else {
+          renderGoods(array);
+        }
+
       });
   }
   //The third - use the function and varables
   links.forEach((link) => {
     link.addEventListener('click', (e) => {
       e.preventDefault();
-      getData();
+      const linkValue = link.textContent;
+      const category = link.dataset.field
+      getData(linkValue, category);
+
     })
   })
 
-  localStorage.setItem('goods', JSON.stringify({ name: 'all' }));
+  if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+    renderGoods(JSON.parse(localStorage.getItem('goods')))
+  }
 
 }
 getGoods();
